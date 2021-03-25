0a6e4ceb682c7827e0940aa71d852c67270ad7a9
wsr <wsr@iMac-Mac006.local>
2021-03-25 12:35:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/worldcinema/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.worldcinema;
+
+public class LoginResponse {
+
+    private int token;
+
+    public int getToken() {
+        return token;
+    }
+
+    public void setToken(int token) {
+        this.token = token;
+    }
+}
---
+++ app/src/main/java/com/example/worldcinema/LoginService.java
@@ -0,0 +1,12 @@
+package com.example.worldcinema;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface LoginService {
+
+    @POST("auth/login")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
+
+}
